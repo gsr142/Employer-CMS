86539371c35d18c59168733e8103e51c297df18e
gsr142 <gsr142@gmail.com>
2023-08-02 16:57:34 -0700

    viewAll function finished, addNewEmployee almost there

--- index.js
+++ index.js
@@ -1,40 +1,62 @@
 // import statements
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host: 'localhost',
+const mysql = require('mysql2');
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
 
-    port: 3302,
+        port: 3306,
 
-    user: 'root',
+        user: 'root',
 
-    password: 'password',
-    database: 'business_db'
-});
+        password: 'password',
+        database: 'business_db'
+    });
 
-connection.connect(function(err) {
+db.connect(function (err) {
     if (err) throw err;
     //start function here
+    
 })
 
 
-const viewAllEmployees = () => {
-    //query database
-    //display results
+const viewAll = (input) => {
+    //query database based on user input. options are department, role, employee
+    db.query(`SELECT * FROM ${input}`, function (err, res){
+        if (err) {
+            console.error();
+        }
+        //display results as a table instead of array of objs
+        console.table(res);
+    });
     //return to main menu
 }
 
-const addNewEmployee = () => {
-
+const addNewEmployee = (input) => {
+    db.query(`INSERT INTO employees (first_name, last_name, role_id, manager_id) 
+    VALUES (${input.first_name}, ${input.last_name}, ${input.role_id}, ${input.manager_id})`, function (err, res){
+        if (err) {
+            console.error();
+        }
+        
+        return res;
+    
+    });
 }
 
 const addNewRole = () => {
     //ask the name of the role
     //ask the salary 
     //query departments
-        //ask department with list of choices
-        //write new data to db
+    //ask department with list of choices
+    //write new data to db
     //return confirmation message
     //return to main menu
 }
 
-// Connect to database
+newEmp = {
+    'first_name': 'Greg',
+    'last_name': 'Richardson',
+    'role_id': 3,
+    'mananger_id': '1'
+}
+addNewEmployee(newEmp)
